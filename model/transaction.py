5b4f0be0b2ec42e4b27a17b2ed5565976cf1c777
Tobias <tobias.aditya@gmail.com>
2021-11-05 15:11:11 +0700

    add id

--- model/transaction.py
+++ model/transaction.py
@@ -1,9 +1,34 @@
-from pydantic import BaseModel
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel, Field
+from bson import ObjectId
+import json
+class ObjectStr(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):    
+        if not v:
+            return v            
+        return str(v)
 
 class TransactionData(BaseModel):
-    requestTime:str = None
     trxType:str
     amount:str
     desc:str
     trxMethod:str
-    trxId:str
+    requestTime:datetime = None
+    trxId:str = None
+
+class TransactionDataDb(TransactionData):
+    id:ObjectStr = Field(... , alias="_id")
+    requestTime : ObjectStr = None
+
+class TransactionDataShow(BaseModel):
+    n_data : int = None
+    total_puchase : int = None
+    total_income  : int = None
+    total_net : int = None
+    content :List[TransactionDataDb] = []
--- route/route_transaction.py
+++ route/route_transaction.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
 from model.model_response import DefaultResponse, DefaultResponseContent
-from model.transaction import TransactionData
+from model.transaction import TransactionData, TransactionDataDb, TransactionDataShow
 
 from config.config import trx_collection
 
-from datetime import datetime
+from datetime import datetime, timezone, tzinfo
 import uuid
 
 
@@ -24,13 +24,14 @@ async def add_transaction(trx_data:TransactionData):
 @router_transaction.get("/get",response_model=DefaultResponseContent)
 async def get_transaction():
     n_data = await trx_collection.count_documents({})
+    total_income = 0
+    total_purchase = 0
     if n_data ==0:
         all_trx = []
         status_code = 404
     else:
-        all_trx = await trx_collection.find({},{"_id":0}).to_list(n_data)
-        total_income = 0
-        total_purchase = 0
+        all_trx = await trx_collection.find({}).to_list(n_data)
+        
         for trx in all_trx:
             if trx['trxType'] == "PURCHASE":
                 total_purchase+=int(trx['amount'])
@@ -39,18 +40,17 @@ async def get_transaction():
 
 
         status_code = 200
+    
 
-    content = {
-        "n_data":n_data,
-        "total_purchase":(total_purchase),
-        "total_income":(total_income),
-        "total_net":(total_income-total_purchase),
-        "datas":all_trx
-        }
+    data = TransactionDataShow(content = all_trx)
+    data.n_data = n_data
+    data.total_income = total_income
+    data.total_puchase = total_purchase
+    data.total_net = total_income - total_purchase
 
     return {
         "request_time":str(datetime.now()),
         "status_code":status_code,
         "message":"Success get transaction data",
-        "content":content
+        "content":data.dict()
     }
